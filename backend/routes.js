5d1afad018e8ac409fd874eec90518a1c04ccc5e
unknown <vinayakhegde69@gmail.com>
2016-04-09 22:48:31 +0530

    node server port , fixed to serve index file
    Signed-off-by: unknown <vinayakhegde69@gmail.com>

--- backend/routes.js
+++ backend/routes.js
@@ -29,7 +29,7 @@ var Nerd = require('./models/nerd');
         // frontend routes =========================================================
         // route to handle all angular requests
         app.get('*', function(req, res) {
-            res.sendfile('./public/views/index.html'); // load our public/index.html file
+            res.sendfile('../app/index.html'); // load our public/index.html file
         });
 
     };
