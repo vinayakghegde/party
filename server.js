43185f37af3fb78e39c519fe1fb1c4b9e5ca2f81
unknown <vinayakhegde69@gmail.com>
2016-04-10 13:59:02 +0530

    connected to mongo db
    Signed-off-by: unknown <vinayakhegde69@gmail.com>

--- server.js
+++ server.js
@@ -6,38 +6,44 @@ var express        = require('express');
 var app            = express();
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
-
+var mongoose = require('mongoose');
 // configuration ===========================================
     
 // config files
 var db = {
-        url : 'mongodb://localhost/stencil-dev'
+        url : 'mongodb://localhost/orders'
     }   ;
 // set our port
-var port = process.env.PORT || 8080; 
+var port = process.env.PORT || 9001; 
 
 // connect to our mongoDB database 
 // (uncomment after you enter in your own credentials in config/db.js)
-// mongoose.connect(db.url); 
+ mongoose.connect(db.url); 
 
 // get all data/stuff of the body (POST) parameters
 // parse application/json 
-app.use(bodyParser.json()); 
+//app.use(bodyParser.json()); 
 
 // parse application/vnd.api+json as json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
+//app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true })); 
+//app.use(bodyParser.urlencoded({ extended: true })); 
 
 // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-app.use(methodOverride('X-HTTP-Method-Override')); 
+//app.use(methodOverride('X-HTTP-Method-Override')); 
 
 // set the static files location /public/img will be /img for users
-app.use(express.static(__dirname + '/app')); 
+//app.use(express.static(__dirname + '/app'));
+
+// Express
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); 
 
 // routes ==================================================
-require('./backend/routes')(app); // configure our routes
+app.use('/api', require('./backend/api'));
+// require('./backend/routes')(app); // configure our routes
 
 // start app ===============================================
 // startup our app at http://localhost:8080
